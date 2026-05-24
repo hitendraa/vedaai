dec67b8e156382ccaf7f255cd5d517e4bd0a4a48
Hitendra <88222037+hitendraa@users.noreply.github.com>
2026-05-24 13:24:02 +0530

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@vedaai/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
